24aa3aff10745755baecbf862265e0fc04505c96
ItzEduhh <drdsebben14@gmail.com>
2025-06-22 12:23:40 -0300

    Finalização dos testes unitários

--- unitarioW.test.js
+++ unitarioW.test.js
@@ -1,6 +1,21 @@
 function runUnitTests() {
 
-    function cadastroCredenciado(cadastroCredenciado) {
+    function cadastroExpositor(cadastroExpositor) {
+        return {
+            cpf: "111.111.111-11",
+        }
+
+    }
+
+    function cadastroPrestador(cadastroPrestador) {
+        return {
+            cnpj: "11.111.111/0001-11"
+        }
+
+    }
+
+
+    function cadastroColaborador(cadastroColaborador) {
         return {
             nome: "Alex Zappani",
             cpf: "111.111.111-11",
@@ -13,16 +28,46 @@ function runUnitTests() {
         return {
             modelo: "Impala 77",
             cor: "Preto",
-            Placa: "LLLNLNN"
+            placa: "LLLNLNN"
         }
     }
 
-    testar("UNIT - Vinculo do cadastro", () => {
-        if (cadastroCredenciado()) throw new Error(".");
-    });
+
+   
+    testar("UNIT - Cadastro do expositor", () => {
+        const expositor = cadastroExpositor()
+        if (!expositor || !expositor.cpf ) 
+        throw new Error("Não existe um expositor")
+
+        if (!validarCPF(expositor.cpf)) {
+        throw new Error("CPF do expositor é inválido");
+        }
+    })
+
+    testar("UNIT - Cadastro do Prestador de Serviço", () => {
+        const prestador = cadastroPrestador()
+        if (!prestador || !prestador.cnpj ) 
+        throw new Error("Não existe um Prestador de Serviço")
+
+        if (!validarCNPJ(prestador.cnpj)) {
+        throw new Error("CNPJ do Prestador de Serviço é inválido");
+        }
+    })
+
+    testar("UNIT - Vínculo do colaborador", () => {
+        const colaborador = cadastroColaborador()
+        if (!colaborador || !colaborador.nome || !colaborador.cpf || !colaborador.funcao) 
+        throw new Error("Não existe um colaborador")
+
+        if (!validarCPF(colaborador.cpf)) {
+        throw new Error("CPF do colaborador é inválido")
+        }
+    })
 
     testar("UNIT - Cadastro do veículo", () => {
-        if (cadastroCredenciado()) throw new Error(".");
-    });
+        const veiculo = cadastroVeiculo()
+        if (!veiculo || !veiculo.modelo || !veiculo.cor || !veiculo.placa ) 
+        throw new Error("Não existe um veículo para estacionar");
+    })
 
 }
